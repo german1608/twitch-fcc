9513cd6375468384f3cd0e252d1d3fde9f0f2084
German Robayo Paz <germanrobayo33@gmail.com>
2017-07-04 11:08:59 -0400

    Starting the request to the twitch API

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+const users = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
+
 $(document).ready(function () {
   // For the navbar
   $(".button-collapse").sideNav();
@@ -7,4 +9,40 @@ $(document).ready(function () {
     swipeable: true
     // responsiveThreshold: $('.app').width()
   });
+  users.forEach(user => {
+    $.ajax({
+      url: 'https://wind-bow.glitch.me/twitch-api/streams/' + user,
+      dataType: 'jsonp'
+    })
+    .done(function(json) {
+      let channel, game, icon;
+      let content = `
+      li>
+        <div class="user row valign-wrapper">
+          <div class="col s4">
+            <img src="assets/images/profile.jpg" alt="profile.jpg" class="responsive-img circle"  />
+          </div>
+          <div class="col s6 left-align">
+            <h5><a href="${channel}">${user}</a></h5>
+            <p>${game}</p>
+          </div>
+          <div class="col s2">
+           ${icon}
+          </div>
+        </div>
+      </li>
+      `;
+      if (json.stream) {
+        console.log(json);
+      }
+    })
+    .fail(function() {
+      console.log("error");
+    })
+    .always(function() {
+      console.log("complete");
+    });
+    
+  })
+  
 });
